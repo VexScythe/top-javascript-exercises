ffff438478a0cc3d4872001dcd6c964725388f3b
VexScythe <davide.ludovici@yahoo.it>
2024-03-03 16:44:53 +0100

    Resolve the second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, nTimes) {
+    let finalString = ``;
+    if (nTimes >= 0){
+        for (let i = 0; i < nTimes; i++){
+            finalString += word;
+        }
+        return finalString;
+    }else return `ERROR`;
+    
 };
 
 // Do not edit below this line
